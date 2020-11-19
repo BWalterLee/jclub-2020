8b26c7b4515d274473392add15be6c180c50352b
Ben Lee <benjamin.w.lee@wsu.edu>
2020-11-19 12:47:36 -0800

    lil fig

--- scripts/food_waste_script.R
+++ scripts/food_waste_script.R
@@ -14,3 +14,7 @@ View(fwaste_data)
 
 mean(fwaste_data$fw_perc, na.rm=T)
   
+fwaste_gdp_plot <- ggplot(data = fwaste_data %>% filter(!is.na(gdp))) +
+  geom_point(mapping= aes(x= log(gdp), y = fw_perc, color = region_id))+
+  theme_classic() + xlab("log(GDP)") + ylab("Perc. Organic Food Waste")
+ggsave("FoodWaste_gdp.png", fwaste_gdp_plot)
