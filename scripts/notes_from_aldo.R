67e306c490cfcdcbba5ce2bcf3df65362288e2c4
Benjamin W Lee <46976076+BWalterLee@users.noreply.github.com>
2021-03-24 11:07:35 -0700

    saving

---
+++ scripts/notes_from_aldo.R
@@ -0,0 +1,29 @@
+# Collab with Aldo
+
+# Paper Structure
+# Open with Refuting Ewers for recent years
+
+# 
+
+# 
+
+# Hypotheses to test
+
+#  - GM cause huge gains in Developing rather than Developed countries
+#  - Yield changes are less predictive of land area changes that other metrics
+#     - Nitrogen and pesticide use, and export values are significant predictors of area change
+#     - However, are highly influenced by/correlated with HDI 
+#  - 
+#  - How have yield gaps closed in recent years, effects over time? Specific crops? What countries?
+#  -    Isolate top producing countries, specific crops, shorter time chunks
+#  - 
+
+# Considerations
+
+# - Setting thresholds on ag area or economic area to remove tiny countries (removes almost exclusively outliers)
+
+# To Do
+#
+# - Create new datasheet with total cropland area value 
+# -   With calories excluded now can focus on yield 
+# -   
