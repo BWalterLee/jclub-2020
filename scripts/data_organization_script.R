64f4e738df421988bb34c9698b7a18fd16c48282
Benjamin W Lee <46976076+BWalterLee@users.noreply.github.com>
2021-03-24 11:07:15 -0700

    Adding staple-only Data

--- scripts/data_organization_script.R
+++ scripts/data_organization_script.R
@@ -2,10 +2,12 @@
 
 # SOLVED PROBLEM WITH MISSING COUNTRIES
 
+# Holy shit Ben you're trash, "Cottonseed" now changed to "Seed cotton" 3/24
+
 staple_data <- read.csv("../data/Production_Crops_E_All_Data_NOFLAG.csv", sep = ",", header = T) %>% 
   dplyr::filter(Area.Code < 1000) %>% 
-  dplyr::filter(Item =="Apples"| Item == "Barley"| Item ==  "Bananas"| Item == "Cassava"| Item == "Coconuts"| Item ==  "Cottonseed"| 
-                  Item ==  "Grapes"| Item ==  "Maize"| Item ==  "Millet"| Item == "Oats"| Item ==  "Onions| Item ==  dry"| Item ==  "Oil palm fruit"| 
+  dplyr::filter(Item =="Apples"| Item == "Barley"| Item ==  "Bananas"| Item == "Cassava"| Item == "Coconuts"| Item ==  "Seed cotton"| 
+                  Item ==  "Grapes"| Item ==  "Maize"| Item ==  "Millet"| Item == "Oats"| Item ==  "Onions, dry"| Item ==  "Oil palm fruit"| 
                   Item ==  "Plantains"| Item ==  "Potatoes"| Item ==  "Rice, paddy"| Item ==  "Sorghum"| 
                   Item == "Soybeans"| Item ==  "Sugar beet"| Item ==  "Sugar cane"| Item ==  "Sunflower seed"| Item ==  "Sweet potatoes"| 
                   Item ==  "Vegetables, fresh nes"| Item == "Wheat"| Item ==  "Yams") %>% 
@@ -20,7 +22,7 @@ head(area.tall)
 yield.tall <- pivot_longer(data = staple_data_clean %>% filter(Element == "Yield"),cols = 8:66, names_to = "Year", values_to = "yield") 
 yield.tall$Year <- as.numeric(yield.tall$Year)
 View(yield.tall)
-kcal = data.frame(c("Apples","Barley", "Bananas","Cassava","Coconuts", "Cottonseed", "Grapes", "Maize", "Millet",
+kcal = data.frame(c("Apples","Barley", "Bananas","Cassava","Coconuts", "Seed cotton", "Grapes", "Maize", "Millet",
                     "Oats", "Onions, dry", "Oil palm fruit", "Plantains", "Potatoes", "Rice, paddy", "Sorghum",
                     "Soybeans", "Sugar beet", "Sugar cane", "Sunflower seed", "Sweet potatoes", "Vegetables, fresh nes",
                     "Wheat", "Yams"),  
@@ -100,6 +102,7 @@ emissions_data <- read.csv("../data/fao_emissions.csv", sep = ",", header =T)%>%
   dplyr::mutate(CO2_eq_emissions = Value) %>% 
   dplyr::select(Area,CO2_eq_emissions,Year)
 
+# Final Merge
 mergeset <- read.csv("../data/mergeset.csv", sep = ",", header = T)
 
 fao_composite_tall <- left_join(pop_data_clean,nitrogen_data) %>% 
@@ -113,5 +116,55 @@ fao_composite_tall <- left_join(pop_data_clean,nitrogen_data) %>%
   left_join(.,mergeset)
 View(fao_composite_tall)
 
+write.csv(fao_composite_tall, "../data/fao_staple_data.csv")
 
-write.csv(fao_composite_tall, "../data/fao_composite_tall.csv")
+# New Data Sheet with specific staple values only where able ####
+
+# Value of Ag Production Staple Only
+staple_production_value <- read.csv("../data/Value_of_Production_E_All_Data_NOFLAG.csv", sep = ",", header = T) %>% 
+  dplyr::filter(Area.Code < 1000, 
+                Element == "Gross Production Value (constant 2014-2016 thousand I$)") %>% 
+  dplyr::filter(Item =="Apples"| Item == "Barley"| Item ==  "Bananas"| Item == "Cassava"| Item == "Coconuts"| Item ==  "Seed cotton"| 
+                  Item ==  "Grapes"| Item ==  "Maize"| Item ==  "Millet"| Item == "Oats"| Item ==  "Onions, dry"| Item ==  "Oil palm fruit"| 
+                  Item ==  "Plantains"| Item ==  "Potatoes"| Item ==  "Rice, paddy"| Item ==  "Sorghum"| 
+                  Item == "Soybeans"| Item ==  "Sugar beet"| Item ==  "Sugar cane"| Item ==  "Sunflower seed"| Item ==  "Sweet potatoes"| 
+                  Item ==  "Vegetables, fresh nes"| Item == "Wheat"| Item ==  "Yams") %>% 
+  dplyr::filter(Element != "Production") 
+names(staple_production_value) = gsub(pattern = "Y", replacement = "", x = names(staple_data))
+staple_value_clean <- staple_production_value
+staple_value_tall <- pivot_longer(data = staple_value_clean,cols = 8:66, names_to = "Year", values_to = "staple.value")%>% 
+  dplyr::select(Area, Item, Year,staple.value)
+staple_value_tall$Year <- as.numeric(staple_value_tall$Year)
+
+staple_value_complete <- staple_value_tall %>% 
+  dplyr::group_by(Area, Year) %>% 
+  dplyr::summarise(mean.staple.value = mean(staple.value, na.rm = T))
+View(staple_value_complete)
+
+# Imports Staple Only
+staple_imports_raw <- read.csv("../data/fao_staple_import_value.csv", sep = ",", header = T) %>% 
+  dplyr::mutate(staple.import.value = Value) %>% 
+  dplyr::select(Area,Item,Year,staple.import.value) %>% 
+  dplyr::group_by(Area,Year) %>% 
+  dplyr::summarise(mean.staple.imports = mean(staple.import.value, na.rm = T))
+
+# exports Staple Only
+staple_exports_raw <- read.csv("../data/fao_staple_export_value.csv", sep = ",", header = T) %>% 
+  dplyr::mutate(staple.export.value = Value) %>% 
+  dplyr::select(Area,Item,Year,staple.export.value) %>% 
+  dplyr::group_by(Area,Year) %>% 
+  dplyr::summarise(mean.staple.exports = mean(staple.export.value, na.rm = T))
+
+fao_staple_complete <- left_join(pop_data_clean,nitrogen_data) %>% 
+  left_join(.,staple_imports_raw) %>% 
+  left_join(.,staple_exports_raw) %>% 
+  left_join(.,staple_value_complete) %>% 
+  left_join(.,area_yield_data) %>% 
+  left_join(.,pest_total_data) %>% 
+  left_join(.,for_invest_data) %>% 
+  left_join(.,emissions_data) %>% 
+  left_join(.,mergeset)
+View(fao_staple_complete)
+
+write.csv(fao_staple_complete, "../data/fao_updated_staple.csv")
+
